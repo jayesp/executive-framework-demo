9098ee559a3b034bf7652272223b72fd63eda2a3
Jay Espiritu <jayespiritu@yahoo.com>
2025-09-17 11:04:50 -0400

    3-6-11 - Locks

--- src/main/java/com/jay/demos/concurrency/DownloadStatus.java
+++ src/main/java/com/jay/demos/concurrency/DownloadStatus.java
@@ -1,7 +1,11 @@
 package com.jay.demos.concurrency;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
     private int totalBytes;
+    private Lock lock = new ReentrantLock();
 
     public int getTotalBytes() {
         return totalBytes;
@@ -10,8 +14,13 @@ public class DownloadStatus {
     public void setTotalBytes(int totalBytes) {
         this.totalBytes = totalBytes;
     }
-    
+
     public void incrementTotalBytes() {
-        totalBytes++;
+        lock.lock();
+        try {
+            totalBytes++;
+        } finally {
+            lock.unlock();
+        }
     }
 }
