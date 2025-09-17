691dcd6ee9f69fdfa6c166b627c194ac0f8420e7
Jay Espiritu <jayespiritu@yahoo.com>
2025-09-17 12:35:38 -0400

    3-6-12 - Synchronized Keyword

--- src/main/java/com/jay/demos/concurrency/DownloadStatus.java
+++ src/main/java/com/jay/demos/concurrency/DownloadStatus.java
@@ -2,16 +2,27 @@ package com.jay.demos.concurrency;
 
 public class DownloadStatus {
     private int totalBytes;
+    private int totalFiles;
+    private Object totalBytesLock = new Object();
+    private Object totalFilesLock = new Object();
 
     public int getTotalBytes() {
         return totalBytes;
     }
 
-    public void setTotalBytes(int totalBytes) {
-        this.totalBytes = totalBytes;
+    public int getTotalFiles() {
+        return totalFiles;
     }
-    
+
     public void incrementTotalBytes() {
-        totalBytes++;
+        synchronized (totalBytesLock) {
+            totalBytes++;
+        }
+    }
+
+    public void incrementTotalFiles() {
+        synchronized (totalFilesLock) {
+            totalFiles++;
+        }
     }
 }
