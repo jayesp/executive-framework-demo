7e36874a940a1ab4767aa31f1c02f2d7f384f699
Jay Espiritu <jayespiritu@yahoo.com>
2025-09-19 08:35:11 -0400

    3-6-17 - Synchronized Collections

--- src/main/java/com/jay/demos/concurrency/ThreadDemo.java
+++ src/main/java/com/jay/demos/concurrency/ThreadDemo.java
@@ -1,28 +1,30 @@
 package com.jay.demos.concurrency;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class ThreadDemo {
     public static void show() {
-        var status = new DownloadStatus();
+        Collection<Integer> collection = new ArrayList<Integer>();
+        Thread thread1 = new Thread(() -> {
+            collection.addAll(Arrays.asList(1, 2, 3));
+        });
+        Thread thread2 = new Thread(() -> {
+            collection.addAll(Arrays.asList(4, 5, 6));
+        });
 
-        var threads = new ArrayList<Thread>();
+        thread1.start();
+        thread2.start();
 
-        for (int i = 0; i < 10; i++) {
-            var thread = new Thread(new DownloadFileTask(status));
-            thread.start();
-            threads.add(thread);
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-        
-        System.out.println(status.getTotalBytes());
+        System.out.println(collection);
     }
 }
