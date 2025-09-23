7d7df3c55e9f39e3a9e88a266488fb5f6a27b541
Jay Espiritu <jayespiritu@yahoo.com>
2025-09-22 20:41:30 -0400

    3-6-07 - CompletableFuture

--- src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
+++ src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
@@ -1,9 +1,20 @@
 package com.jay.demos.executors;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 
 public class CompletableFuturesDemo {
     public static void show() {
-        CompletableFuture.runAsync(null);
+        Supplier<Integer> task = () -> 1;
+        var future = CompletableFuture.supplyAsync(task);
+        try {
+            var result = future.get();
+            System.out.println(result);
+        } catch (InterruptedException | ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 }
