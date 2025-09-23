6f41efb48db10421be6c7b72d71db13cfeb31e2f
Jay Espiritu <jayespiritu@yahoo.com>
2025-09-22 22:47:24 -0400

    3-6-11 - Transforming a Completable Future (simplified)

--- src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
+++ src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
@@ -1,7 +1,6 @@
 package com.jay.demos.executors;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class CompletableFuturesDemo {
     public static int toFahrenheit(int celsius) {
@@ -11,16 +10,9 @@ public class CompletableFuturesDemo {
     public static void show() {
         var future = CompletableFuture.supplyAsync(() -> 20);
 
-        try {
-            var result = future.thenApply(CompletableFuturesDemo::toFahrenheit).get();
-            System.out.println(result);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        future
+            .thenApply(CompletableFuturesDemo::toFahrenheit)
+            .thenAccept(f -> System.out.println(f));
 
         System.out.println("show() ended");
     }
