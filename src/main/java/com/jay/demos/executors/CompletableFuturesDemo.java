4628e39fad1e99cc543e56d615c365d96d28ba89
Jay Espiritu <jayespiritu@yahoo.com>
2025-09-23 14:37:48 -0400

    3-6-14 - Waiting for Many Tasks to Complete

--- src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
+++ src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
@@ -1,21 +1,29 @@
 package com.jay.demos.executors;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class CompletableFuturesDemo {
 
     public static void show() {
-        var first = CompletableFuture
-                .supplyAsync(() -> "20USD")
-                .thenApply(str-> {
-                    var price = str.replace("USD", "");
-                    return Integer.parseInt(price);
-                });
-        var second = CompletableFuture.supplyAsync(() -> 0.9);
+        var first = CompletableFuture.supplyAsync(() -> 1);
+        var second = CompletableFuture.supplyAsync(() -> 2);
+        var third = CompletableFuture.supplyAsync(() -> 3);
 
-        first
-            .thenCombine(second, (price, exchangeRate) -> price * exchangeRate)
-            .thenAccept(result -> System.out.println(result));
+        var all = CompletableFuture.allOf(first, second, third);
+        all.thenRun(() -> {
+            try {
+                var firstResult = first.get();
+                System.out.println(firstResult);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("All tasks completed successfully");
+        });
 
         System.out.println("show() ended");
     }
