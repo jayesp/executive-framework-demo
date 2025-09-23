d0d30bab73b4d0dcd19402f3a87f521ba8d0d96d
Jay Espiritu <jayespiritu@yahoo.com>
2025-09-23 16:36:55 -0400

    3-6-15 - Waiting for the First Task

--- src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
+++ src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
@@ -1,29 +1,20 @@
 package com.jay.demos.executors;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class CompletableFuturesDemo {
 
     public static void show() {
-        var first = CompletableFuture.supplyAsync(() -> 1);
-        var second = CompletableFuture.supplyAsync(() -> 2);
-        var third = CompletableFuture.supplyAsync(() -> 3);
-
-        var all = CompletableFuture.allOf(first, second, third);
-        all.thenRun(() -> {
-            try {
-                var firstResult = first.get();
-                System.out.println(firstResult);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            System.out.println("All tasks completed successfully");
+        var first = CompletableFuture.supplyAsync(() -> {
+            LongTask.simulate();
+            return 20;
         });
+
+        var second = CompletableFuture.supplyAsync(() -> 20);
+
+        var fastest = CompletableFuture
+            .anyOf(first, second)
+            .thenAccept(temp -> System.out.println(temp));
 
         System.out.println("show() ended");
     }
