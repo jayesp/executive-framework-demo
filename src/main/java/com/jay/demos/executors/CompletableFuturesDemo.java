dea58ccb54999c59c5ad10e13e2dd813ca9343f5
Jay Espiritu <jayespiritu@yahoo.com>
2025-09-23 13:48:05 -0400

    3-6-13 - Combining Completable Futures
    (with transformation)

--- src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
+++ src/main/java/com/jay/demos/executors/CompletableFuturesDemo.java
@@ -5,7 +5,12 @@ import java.util.concurrent.CompletableFuture;
 public class CompletableFuturesDemo {
 
     public static void show() {
-        var first = CompletableFuture.supplyAsync(() -> 20);
+        var first = CompletableFuture
+                .supplyAsync(() -> "20USD")
+                .thenApply(str-> {
+                    var price = str.replace("USD", "");
+                    return Integer.parseInt(price);
+                });
         var second = CompletableFuture.supplyAsync(() -> 0.9);
 
         first
